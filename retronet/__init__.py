2f915b0353105823fc54085064d432fa02687e0c
Mikolaj Kowalik <mxk62@psu.edu>
2014-05-13 11:16:23 -0400

    Make modules visible on package level
    Newly created modules were not visible on package level. I modified
    __init_I.py to fix the issue.

--- retronet/__init__.py
+++ retronet/__init__.py
@@ -1,2 +1,8 @@
 from chemical import Chemical
+from pattern import Pattern
 from transform import Transform
+from building import *
+from depgraph import *
+from statistics import *
+
+EPSILON = 1.0e-06
