5115d1fd43e1331b594d61dae89f8d9f71f15cb0
Mikolaj Kowalik <mxk62@psu.edu>
2014-07-07 12:04:42 -0400

    Improve comments in count_nodes
    I made minor changes mainly in function docstring and comments to make
    it more clear.

--- retronet/statistics.py
+++ retronet/statistics.py
@@ -2,7 +2,7 @@ import collections
 
 
 def count_nodes(graph, root, max_depth):
-    """Counts nodes at given depth.
+    """Counts nodes at given depth range.
 
     Parameters
     ----------
@@ -11,27 +11,28 @@ def count_nodes(graph, root, max_depth):
     root : a graph node
         A seed node for which counting is done.
     max_depth : integer
-        Maximal depth for which counting is performed.
+        Maximal depth for which nodes are counted.
 
     Returns
     -------
-    count : dictionary
-        A dictionary which keys represents depths and values number of nodes at
-        that depth.
+    counts : dictionary
+        A dictionary which keys represents depths (node distances) from the
+        root and their values the total number of nodes at a given distance.
     """
 
-    # Usually we measure depth in synthetic steps (no. of reactions
-    # separating two chemicals, but here we want to mean it what it usually
-    # does -- a step between adjacent nodes.
+    # Usually we measure depth in synthetic steps (no. of reactions separating
+    # two chemicals), but here we want to mean it what it usually does --
+    # a distance between adjacent nodes.
     max_depth *= 2
 
     # Initialize counters (values) at given depths (keys).
-    occupation = {depth: 0 for depth in range(max_depth)}
+    counts = {depth: 0 for depth in range(max_depth + 1)}
 
     discovered = {root}
     queue = collections.deque([(root, 0)])
     while queue:
         child, depth = queue.popleft()
+        counts[depth] += 1
 
         # Do not procceed beyond a given depth.
         if depth == max_depth:
@@ -41,10 +42,7 @@ def count_nodes(graph, root, max_depth):
         parents = set(graph.predecessors(child))
         undiscovered = parents - discovered
 
-        # Update counters.
-        occupation[depth] += len(undiscovered)
-
         # Enqueue newly discovered nodes.
         queue.extend([(u, depth + 1) for u in undiscovered])
         discovered |= undiscovered
-    return occupation
+    return counts
