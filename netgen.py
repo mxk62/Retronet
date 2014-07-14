e81a08d78dbfcb9fbe1baf0dc7c383f1f091e775
Mikolaj Kowalik <mxk62@psu.edu>
2014-07-14 17:59:01 -0400

    Introduce log file
    All events were directed to stdout.  Now the script using log file
    instead allowing for more fine-grained management of events which are
    reported.

--- netgen.py
+++ netgen.py
@@ -1,23 +1,32 @@
 import argparse
+import logging
+import networkx
 import pickle
 import time
-import networkx
 import retronet as rn
 
 
 # Parse command line arguments.
 parser = argparse.ArgumentParser()
-parser.add_argument('-d', '--depth', type=int, default=3,
+parser.add_argument('-d', '--depth', type=int, default=1,
                     help='Maximal network depth.')
-parser.add_argument('-t', '--transforms', type=str, default='cores.dat',
-                    help='File with available transforms.')
 parser.add_argument('-p', '--popularity', type=int, default=5,
                     help='Core popularity threshold.')
+parser.add_argument('-g', '--graph', type=str, default='smart',
+                    help='Core popularity threshold.')
+parser.add_argument('-t', '--transforms', type=str, default='cores.dat',
+                    help='File with available transforms.')
 parser.add_argument('file', type=str,
                     help='File with chemicals SMILES.')
 args = parser.parse_args()
 
+# Initialize a logger.
+logging.basicConfig(filename='netgen.log',
+                    level=logging.INFO,
+                    format='%(name)s: %(levelname)s: %(message)s')
+
 # Acquire all valid, single-retron transforms.
+start = time.clock()
 with open(args.transforms) as f:
     data = pickle.load(f)
 
@@ -31,7 +40,11 @@ for smarts, rxns in data.items():
             continue
         if len(t.retrons) == 1:
             transforms.append(t)
+eta = time.clock() - start
+logging.info('{0} transforms acquired in {1} s.'.format(len(transforms), eta))
 
+# Divide transforms into groups sharing the same retron.
+start = time.clock()
 cores = {}
 patterns = set()
 for t in transforms:
@@ -41,12 +54,17 @@ for t in transforms:
         continue
     patterns.add(patt)
     cores.setdefault(patt.smiles, []).append(t)
+eta = time.clock() - start
+logging.info('Transforms divided into {0} patterns '
+             'in {1} s.'.format(len(patterns),  eta))
 
-if False:
+start = time.clock()
+if args.graph == 'smart':
+    trans_graph = rn.create_depgraph(patterns)
+else:
     trans_graph = networkx.DiGraph()
     trans_graph.add_nodes_from([p for p in patterns])
-else:
-    trans_graph = rn.create_depgraph(patterns)
+eta = time.clock() - start
 
 # Having sequences as node attributes make write_gexf() fail, thus we are
 # making a copy of the graph with lists of duplicates stripped.
@@ -59,11 +77,14 @@ for v in g:
 
 # Write down the dependency graph.
 networkx.write_gexf(g, 'depgraph.gexf')
+g.clear()
+logging.info('Dependency graph created and saved in {0} s.'.format(eta))
 
 # Write down example chemical and its descendants.
-chem_env = networkx.bfs_tree(g, 'c1ccccc1')
-networkx.write_gexf(chem_env, 'benzene.gexf')
+#chem_env = networkx.bfs_tree(g, 'c1ccccc1')
+#networkx.write_gexf(chem_env, 'benzene.gexf')
 
+start = time.clock()
 # Associate transforms with graph patterns
 for patt in trans_graph.nodes():
     trans_graph.node[patt]['transforms'] = cores[patt.smiles]
@@ -73,8 +94,11 @@ for patt in trans_graph.nodes():
         del trans_graph[patt]['duplicates']
     except KeyError:
         continue
+eta = time.clock() - start
+logging.info('Transforms added to graph in {0} s.'.format(eta))
 
 # Read the SMILES from an external file.
+logging.info('Starting building networks for targets...')
 with open(args.file) as f:
     smiles = [line.strip() for line in f.readlines()]
 
@@ -86,15 +110,17 @@ for idx, smi in enumerate(smiles):
     network = rn.populate(smi, trans_graph, depth=args.depth)
     #network = rn.recreate(smi, db, depth=args.depth)
     eta = time.clock() - start
+    logging.info('Finished building for {}.'.format(smi))
 
     # Gather statistics and write them down to a file.
     stats = rn.count_nodes(network, smi, args.depth)
     line = '\t'.join([str(stats[key]) for key in sorted(stats)])
-    with open('occupation.dat', 'a') as f:
+    with open('counts.dat', 'a') as f:
         f.write('{0}\t{1}\t{2}\n'.format(idx, eta, line))
 
     # Write graph for debugging purposes
-    networkx.write_gexf(network, 'testgraph.gexf')
+    #networkx.write_gexf(network, 'testgraph.gexf')
 
     # Discard current network entirely.
     network.clear()
+logging.info('All builds finished.')
